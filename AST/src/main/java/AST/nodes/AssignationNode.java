7a8de95afc92ae6a5880d48a36bc51a6f4c86d30
TomasSerra <tomi.serra@gmail.com>
2024-08-03 17:23:10 -0300

    Semantic Visitor

--- AST/src/main/java/AST/nodes/AssignationNode.java
+++ AST/src/main/java/AST/nodes/AssignationNode.java
@@ -3,15 +3,15 @@ package AST.nodes;
 import AST.ASTVisitor;
 
 public class AssignationNode implements ASTNode {
-    private final ASTNode declaration;
+    private final DeclarationNode declaration;
     private final ASTNode expression;
 
-    public AssignationNode(ASTNode declaration, ASTNode expression) {
+    public AssignationNode(DeclarationNode declaration, ASTNode expression) {
         this.declaration = declaration;
         this.expression = expression;
     }
 
-    public ASTNode getDeclaration() {
+    public DeclarationNode getDeclaration() {
         return declaration;
     }
 
