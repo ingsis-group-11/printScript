4f986c825360f2b99d62a13f9157aaa88c3105d8
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-08-28 23:15:39 -0300

    added formatter to cli

--- cli/src/main/java/cli/commands/FormattingCommand.java
+++ cli/src/main/java/cli/commands/FormattingCommand.java
@@ -1,19 +1,32 @@
 package cli.commands;
 
+import formatter.FormatterRunner;
+import linter.LinterRunner;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 @Command(name = "format", description = "Format a printScript file using a config file")
 public class FormattingCommand implements Runnable {
 
-  @Parameters(index = "0", description = "The source file to format")
-  private String sourceFile;
+  @Parameters(index = "0", description = "The input source file to format")
+  private String inputFile;
 
-  @Parameters(index = "1", description = "The config file that have the formatter rules")
+  @Parameters(index = "1", description = "The output source file to format")
+  private String outputFile;
+
+  @Parameters(index = "2", description = "The config file that have the formatter rules")
   private String configFile;
 
   @Override
   public void run() {
-    System.out.println("Formatting...");
+    System.out.println("Formatting file...");
+    try {
+      FormatterRunner formatterRunner = new FormatterRunner();
+      formatterRunner.format(inputFile, outputFile, configFile);
+      System.out.println("File has been formatted :)");
+    } catch (Exception e) {
+      System.err.print(e);
+      System.exit(1);
+    }
   }
 }
