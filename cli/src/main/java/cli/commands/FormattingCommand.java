69fcb466582858622d414e1861aa446e814b2250
franciscolang <francisco.lang2209@gmail.com>
2024-09-02 10:45:56 -0300

    Changed the CLI formatter command

--- cli/src/main/java/cli/commands/FormattingCommand.java
+++ cli/src/main/java/cli/commands/FormattingCommand.java
@@ -11,7 +11,10 @@ public class FormattingCommand implements Runnable {
   @Parameters(index = "0", description = "The input source file to format")
   private String inputFile;
 
-  @Parameters(index = "1", description = "The config file that have the formatter rules")
+  @Parameters(index = "1", description = "The output file that will be formatted")
+  private String outputFile;
+
+  @Parameters(index = "2", description = "The config file that have the formatter rules")
   private String configFile;
 
   @Override
@@ -19,7 +22,7 @@ public class FormattingCommand implements Runnable {
     System.out.println("Formatting file...");
     try {
       FormatterRunner formatterRunner = new FormatterRunner();
-      FileWriter fileWriter = new FileWriter(inputFile);
+      FileWriter fileWriter = new FileWriter(outputFile);
       formatterRunner.format(inputFile, configFile, fileWriter);
       System.out.println("File has been formatted :)");
     } catch (Exception e) {
