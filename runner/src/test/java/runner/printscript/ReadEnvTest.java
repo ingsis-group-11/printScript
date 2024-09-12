ef61867d0662fcc1cf275f854598f37064bb4cbf
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-09-12 16:12:56 -0300

    mati breaking things

--- runner/src/test/java/runner/printscript/ReadEnvTest.java
+++ runner/src/test/java/runner/printscript/ReadEnvTest.java
@@ -90,9 +90,6 @@ public class ReadEnvTest {
     assertThrows(
         RuntimeException.class,
         () -> {
-          runner.run(
-              new FileInputStream("src/test/resources/readEnv/assignationToBooleanReadEnvFail.txt"),
-              "1.1");
         });
   }
 }
